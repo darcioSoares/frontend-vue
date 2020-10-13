7e50365c32fcb7aeb945133a1715a374843d2a52
darcioSoares <darciosoares28@gmail.com>
2020-10-13 17:28:44 -0300

    update file config App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Layout />
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Layout from '@/components/templates/Layout'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Layout
   }
 }
 </script>
 
 <style>
+*{
+  margin:0px;
+  padding:0px;
+  box-sizing: border-box;
+}
+:root{
+  --green: #1DE9B6;
+  --grey: #9E9E9E;
+  --blueBaby: #00E5FF;
+  --greenGram: #66BB6A;
+  --darklight:#1F2D40;
+  --dark:#17212F;
+  --clearGreen:#69F0AE;
+  --discord: #6e86d6;
+
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  height: 100vh;
+ 
 }
 </style>
