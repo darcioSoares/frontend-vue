896d7edf64ec9992bd438c884a33bae6beb06feb
darcioSoares <darciosoares28@gmail.com>
2020-10-13 22:49:48 -0300

    add Animate.css in Main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'animate.css/animate.min.css'
+
+import store from '@/store/store'
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  store,
 }).$mount('#app')
